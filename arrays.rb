d83ab42bbb8aa28aec67b4701dda7da86a5c17f7
Darby Smith <newman.darby2@gmail.com>
2022-08-03 11:51:58 -0400

    Add data type arrays

---
+++ arrays.rb
@@ -0,0 +1,15 @@
+# Strings
+
+dog_breeds = ["Jack Russell", "Corgi", "Rottweiler", "German Shepard"]
+
+# Integers
+
+age = [8, 5, 3, 12]
+
+# Floats
+
+price = [25.99, 16.47, 12.50, 97.82]
+
+# Booleans
+
+certified = [false, true, true, false]
