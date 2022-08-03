90ca94e0e8f081f0f928758a34bd8430c33f30c3
Darby Smith <newman.darby2@gmail.com>
2022-08-03 12:08:37 -0400

    Add index position examples

--- arrays.rb
+++ arrays.rb
@@ -1,30 +1,42 @@
-# Strings
+# STRINGS
 
 dog_breeds = ["Jack Russell", "Corgi", "Rottweiler", "German Shepard"]
 
 # this removes the first item in the array Jack Russell
 puts dog_breeds.shift
 
+# Index postion example (shows position 2, which is Rottweiler)
+puts dog_breeds[2]
 
-# Integers
+
+# INTEGERS
 
 age = [8, 5, 3, 12]
 
 # this removed the last item in the array 12
 puts age.pop
 
+# Index position example (shows position 1 which is 5 in the array)
+puts age[1]
 
-# Floats
+
+# FLOATS
 
 price = [25.99, 16.47, 12.50, 97.82]
 
 # this adds 13.59 to the beginning of the array
 puts price.unshift(13.59)
 
+# Index position examples (shows position 3 or 97.82)
+puts price[3]
 
-# Booleans
+
+# BOOLEANS
 
 certified = [false, true, true, false]
 
 # this adds true to the end of the array
 puts certified.push(true)
+
+# Index position example (shows the 0 position, false)
+puts certified[0]
