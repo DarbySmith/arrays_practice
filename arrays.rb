6e19620db379cca43d66e3c3c6f61d0693aab076
Darby Smith <newman.darby2@gmail.com>
2022-08-03 12:05:36 -0400

    Add array methods and explanations

--- arrays.rb
+++ arrays.rb
@@ -2,14 +2,29 @@
 
 dog_breeds = ["Jack Russell", "Corgi", "Rottweiler", "German Shepard"]
 
+# this removes the first item in the array Jack Russell
+puts dog_breeds.shift
+
+
 # Integers
 
 age = [8, 5, 3, 12]
+
+# this removed the last item in the array 12
+puts age.pop
+
 
 # Floats
 
 price = [25.99, 16.47, 12.50, 97.82]
 
+# this adds 13.59 to the beginning of the array
+puts price.unshift(13.59)
+
+
 # Booleans
 
 certified = [false, true, true, false]
+
+# this adds true to the end of the array
+puts certified.push(true)
